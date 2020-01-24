76bf4b4b7b1f798354454b666294d4ca81ba3f83
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 03:08:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each_with_index do |item, index|
-    puts "#{index}. #{item}"
+    puts "#{index-1}. #{item}"
   end
 end
 
