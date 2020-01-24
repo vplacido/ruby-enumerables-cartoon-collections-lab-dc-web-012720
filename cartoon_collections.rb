b812acac4db27d90eb6d25e358b397ca7894279f
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 03:14:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  
+  planeteer_calls.map do |item|
+    
 end
 
 def long_planeteer_calls# code an argument here
