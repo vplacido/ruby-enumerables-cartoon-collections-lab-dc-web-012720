8c353d02a4a2842a2b9b94d8c166527076f593ac
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 03:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |index|
+    puts index
+  end
 end
 
 def summon_captain_planet# code an argument here
