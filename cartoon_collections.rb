826aa565eaa6a0e5d50ae3cc0c3a66e1b2aff483
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 03:30:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map {|i| i.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.map {|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
