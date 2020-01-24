1c5e5e17fe400f7acbb9cb8a048727c5998ea3fe
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 03:43:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find
 end
